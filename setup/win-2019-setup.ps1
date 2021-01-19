3d130b35439319a8d012080a03ec3b2f03d63f4f
jonathan-yin2020 <64214379+jonathan-yin2020@users.noreply.github.com>
2021-01-19 14:23:41 -0700

    Update win-2019-setup.ps1

--- setup/win-2019-setup.ps1
+++ setup/win-2019-setup.ps1
@@ -1,7 +1,5 @@
-$DOCKER_VERSION="19.03.0"
-
 # disable security :)
-Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -name Shell -Value 'PowerShell.exe -noExit'
+# Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -name Shell -Value 'PowerShell.exe -noExit'
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Uninstall-WindowsFeature Windows-Defender
 
@@ -9,6 +7,6 @@ Uninstall-WindowsFeature Windows-Defender
 Install-WindowsFeature -Name Containers
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Install-Module -Name DockerMsftProvider -Repository PSGallery -Force
-Install-Package -Name docker -ProviderName DockerMsftProvider -Force -RequiredVersion $DOCKER_VERSION
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force 
 
 Restart-Computer -Force
